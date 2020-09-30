da4594a1df458955da9170f9e224db4111614ede
aimetz <67288550+aimetz@users.noreply.github.com>
2020-09-29 18:03:29 -0700

    Update stacks.py
    Stacks

--- stacks.py
+++ stacks.py
@@ -19,15 +19,56 @@ class StackArray:
 
     def pop(self):
         if self.num_items > 0:
-            temp = self.items[self.num_items-1]
-            self.items[self.num_items-1] = None
+            self.num_items -= 1
+            return self.items[self.num_items]
+        else:
+            raise ValueError
+
+    def peek(self):
+        return self.items[self.num_items-1]
+
+    def size(self):
+        return self.num_items
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class StackLinked:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.top = None
+        self.num_items = 0
+
+    def is_empty(self):
+        return self.num_items == 0
+
+    def is_full(self):
+        return self.num_items == self.capacity
+
+    def push(self, item):
+        if not self.is_full():
+            temp = self.top
+            self.top = Node(item)
+            self.top.next = temp
+            self.num_items += 1
+        else:
+            raise ValueError
+
+    def pop(self):
+        if self.num_items > 0:
+            temp = self.top.data
+            self.top = self.top.next
             self.num_items -= 1
             return temp
         else:
             raise ValueError
 
     def peek(self):
-        return self.items[self.num_items-1]
+        return self.top.data
 
     def size(self):
         return self.num_items
